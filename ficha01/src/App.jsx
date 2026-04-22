89c6d530d34d0b20c64309d402fb732cbebd64f0
Oleksii113 <olvelykyi23ig@epms.pt>
2026-04-22 09:19:34 +0000

    update

--- ficha01/src/App.jsx
+++ ficha01/src/App.jsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 function App() {
   const [temaEscuro, setTemaEscuro] = useState(false);
   const estilos = {
-    backgroundColor: temaEscuro ? "#"
+    backgroundColor: temaEscuro ? "#1f2937" : "#f3f4f6",
+    color: temaEscuro ? "#f9fafb" : "#111827",
+  };
   return(
     <main>
       <h1>Hello lindinios!</h1>
       <p>O Pedro é....</p>
+      <buttton onClick={() => setTemaEscuro(!temaEscuro)}>
+        Mudar Tema
+      </buttton>
     </main>
   );
 }
 
-export default App;
+export default App; 
