22dc4d56cd085e75f96951e9847683a5196b2f49
Oleksii113 <olvelykyi23ig@epms.pt>
2026-04-22 10:41:17 +0000

    update

--- ficha01/src/App.jsx
+++ ficha01/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     event.preventDefault();
     if (!nome) return;
     setNome(nome);
-    setNomes((nomesAnteriores) => [...nomesAnteriores, nome, ultimoNome]);
+    setNomes((nomesAnteriores) => [...nomesAnteriores, nome + ultimoNome]);
     setNome("");
     if (!ultimoNome) return;
     setNome(ultimoNome);
